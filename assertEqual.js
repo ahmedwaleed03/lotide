7df94398d98937bd9eca91e0f17ca813a8045f21
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-21 18:34:24 -0400

    Improved assertEqual with Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // assertEqual function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
