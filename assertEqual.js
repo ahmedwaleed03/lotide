7e37a66635eac59615fc707c12b29e24ddf2fe7d
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-21 18:22:32 -0400

    Created the assertEqual functions

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// assertEqual function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+// testing the function
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
