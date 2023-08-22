788b2f5cf69643e6b0f76bfd502e6c077fe49940
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-22 17:26:54 -0400

    implemented assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+// eqArrays function implementation
+const eqArrays = function(arr1, arr2) {
+  // check if array lengths are the same
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // iterate through both arrays to ensure each element is the same
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // return false if any variable is different
+      return false;
+    }
+  }
+
+  // return true if all variables are equeal
+  return true;
+};
+
+// assertArraysEqual function implementation
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
