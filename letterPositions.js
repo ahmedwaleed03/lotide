0ec545ac07e8d89d6959b038843af78cf8b93319
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-23 17:00:30 -0400

    implemented the letterPositions function

---
+++ letterPositions.js
@@ -0,0 +1,49 @@
+// eqArrays function implementation
+const eqArrays = function(arr1, arr2) {
+  // check if array lengths are the same
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // iterate through both arrays to ensure each element is the same
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      // return false if any variable is different
+      return false;
+    }
+  }
+
+  // return true if all variables are equeal
+  return true;
+};
+
+// assertArraysEqual function implementation
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  
+  // loop through the sentence
+  for (let i = 0; i < sentence.length; i++) {
+    // skip iteration if letter is space
+    if(sentence[i] === ' ') {
+      continue;
+    }
+    if (results[sentence[i]] === undefined) {
+      results[sentence[i]] = [i];
+    } else {
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+// testing the code
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
